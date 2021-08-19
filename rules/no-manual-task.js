57b54b0d4dd0968677c220a73773d00cfaa87778
Felix Mueller <>
2021-08-19 20:09:32 +0200

    Add type linting for scriptTask,sendTask,serviceTask + clean messages

--- rules/no-manual-task.js
+++ rules/no-manual-task.js
@@ -10,7 +10,7 @@ module.exports = function() {
 
   function check(node, reporter) {
     if (is(node, 'bpmn:ManualTask')) {
-      reporter.report(node.id, 'Unsupported task type bpmn:ManualTask');
+      reporter.report(node.id, 'This Task Type cannot be executed. Click on the Wrench icon to change the type.');
     }
   }
 
